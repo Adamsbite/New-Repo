c1cb19f0a8cab6a28e1d18ace6e06a8d3546efab
bright adams <btadamz@gmail.com>
2024-05-15 07:49:09 +0100

    added some annonymous functions

---
+++ anonymous.js
@@ -0,0 +1,19 @@
+// example 
+
+var greet = function() {
+    console.log("Welcome to GeeksforGeeks!");
+};
+greet(); // Outputs: Welcome to GeeksforGeeks!
+
+
+// example 2
+var greet = function(platform) {
+    console.log("Welcome to ", platform);
+};
+greet("GeeksforGeeks!"); // Outputs: Welcome to GeeksforGeeks!
+
+// example 3 
+var calculateArea = function(radius, height) {
+    return Math.PI * radius * radius * height;
+};
+console.log(calculateArea(5, 10)); // Outputs: 785.3981633974483
